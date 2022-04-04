e4df43c437010e792d9e07861f71739b19050583
Mudit Khanna <muditkhanna998@gmail.com>
2022-04-04 19:31:11 -0400

    Adds form validation using jquery

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,63 @@ span.onclick = function () {
 setTimeout(() => {
   document.getElementsByClassName("modal")[0].style.display = "block";
 }, 2500);
+
+$(document).ready(() => {
+  $("#submit").click(() => {
+    const mobilePattern = new RegExp(/^\d{10}$/);
+    const emailPattern = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
+    const firstName = $("#firstName").val();
+    const lastName = $("#lastName").val();
+    const mobile = $("#mobile").val();
+    const email = $("#email").val();
+    const from = $("#from").val();
+    let isValid = true;
+
+    // validate the first name
+    if (firstName === "" || firstName === undefined) {
+      $("#firstName").next().text("This field is required.");
+      isValid = false;
+    } else {
+      $("#firstName").next().text("");
+    }
+
+    // validate the last name
+    if (lastName === "" || lastName === undefined) {
+      $("#lastName").next().text("This field is required.");
+      isValid = false;
+    } else {
+      $("#lastName").next().text("");
+    }
+
+    // validate the mobile
+    if (!mobilePattern.test(mobile)) {
+      $("#mobile").next().text("Enter mobile number in correct format");
+      isValid = false;
+    } else {
+      $("#mobile").next().text("");
+    }
+
+    //validate the email
+    if (!emailPattern.test(email)) {
+      $("#email").next().text("Enter correct email address");
+      isValid = false;
+    } else {
+      $("#email").next().text("");
+    }
+
+    // validate the from
+    if (from === "" || from === undefined) {
+      $("#from").next().text("This field is required.");
+      isValid = false;
+    } else {
+      $("#from").next().text("");
+    }
+
+    // submit the form if all entries are valid
+    if (isValid) {
+      $("#agentForm").submit();
+      $("#agentForm").toggle();
+      $("#submitHeading").show();
+    }
+  });
+});
